5abef9b6afeb39261489415a6f6f35bbff41cd79
Ganesh239239 <esportszen239@gmail.com>
2025-12-28 19:59:27 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,6 @@
-// FILTERS
-const filters = document.querySelectorAll('.filter');
-const cards = document.querySelectorAll('.tool-card');
+const menuBtn = document.getElementById("menuBtn");
+const mobileNav = document.getElementById("mobileNav");
 
-filters.forEach(btn => {
-  btn.addEventListener('click', () => {
-    document.querySelector('.filter.active').classList.remove('active');
-    btn.classList.add('active');
-
-    const type = btn.dataset.filter;
-
-    cards.forEach(card => {
-      card.style.display =
-        type === 'all' || card.classList.contains(type)
-          ? 'flex'
-          : 'none';
-    });
-  });
-});
-
-// MOBILE MENU
-const menuBtn = document.getElementById('menuBtn');
-const mobileNav = document.getElementById('mobileNav');
-
-menuBtn.addEventListener('click', () => {
-  mobileNav.classList.toggle('show');
+menuBtn.addEventListener("click", () => {
+  mobileNav.classList.toggle("show");
 });
