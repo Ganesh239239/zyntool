54f12fa75f296fee24628a82cf40b69fe7331f64
Ganesh239239 <esportszen239@gmail.com>
2025-12-28 20:55:19 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => {
+const menuBtn = document.getElementById("menuBtn");
+const mobileNav = document.getElementById("mobileNav");
 
-  /* MOBILE NAV */
-  const menuBtn = document.getElementById("menuBtn");
-  const mobileNav = document.getElementById("mobileNav");
-
-  menuBtn.addEventListener("click", () => {
-    mobileNav.classList.toggle("show");
-  });
-
-  /* CATEGORY FILTER */
-  const buttons = document.querySelectorAll(".categories button");
-  const cards = document.querySelectorAll(".tool-card");
-
-  buttons.forEach(btn => {
-    btn.addEventListener("click", () => {
-      buttons.forEach(b => b.classList.remove("active"));
-      btn.classList.add("active");
-
-      const filter = btn.dataset.filter;
-
-      cards.forEach(card => {
-        card.style.display =
-          filter === "all" || card.dataset.category === filter
-            ? "flex"
-            : "none";
-      });
-    });
-  });
-
+menuBtn.addEventListener("click", () => {
+  mobileNav.style.display =
+    mobileNav.style.display === "flex" ? "none" : "flex";
 });
