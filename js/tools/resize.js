f9229897ba152a85b0a84d7c8f731bcd6fa2a099
Ganesh239239 <esportszen239@gmail.com>
2025-12-29 18:24:42 +0530

    Update resize.js

--- js/tools/resize.js
+++ js/tools/resize.js
@@ -1,57 +1,48 @@
 const uploadArea = document.getElementById("uploadArea");
 const fileInput = document.getElementById("fileInput");
-const selectBtn = document.getElementById("selectBtn");
 const resizeBtn = document.getElementById("resizeBtn");
 const results = document.getElementById("results");
 
 let files = [];
-let ratio = 1;
 
 uploadArea.onclick = () => fileInput.click();
-selectBtn.onclick = e => {
-  e.stopPropagation();
-  fileInput.click();
-};
 
-fileInput.onchange = e => {
+fileInput.addEventListener("change", e => {
   files = [...e.target.files];
-};
+});
 
 resizeBtn.onclick = () => {
   results.innerHTML = "";
-  files.forEach(file => resizeImage(file));
+  files.forEach(file => process(file));
 };
 
-function resizeImage(file) {
+function process(file) {
   const img = new Image();
   const reader = new FileReader();
 
   reader.onload = e => img.src = e.target.result;
 
   img.onload = () => {
-    const wInput = document.getElementById("widthInput").value;
-    const hInput = document.getElementById("heightInput").value;
+    const wVal = document.getElementById("widthInput").value;
+    const hVal = document.getElementById("heightInput").value;
     const lock = document.getElementById("lockRatio").checked;
 
-    let w = wInput || img.width;
-    let h = hInput || img.height;
+    let w = wVal || img.width;
+    let h = hVal || img.height;
 
-    if (lock && wInput && !hInput) h = Math.round(w * img.height / img.width);
-    if (lock && hInput && !wInput) w = Math.round(h * img.width / img.height);
+    if (lock && wVal && !hVal) h = Math.round(w * img.height / img.width);
+    if (lock && hVal && !wVal) w = Math.round(h * img.width / img.height);
 
-    const canvas = document.createElement("canvas");
-    canvas.width = w;
-    canvas.height = h;
-    canvas.getContext("2d").drawImage(img, 0, 0, w, h);
+    const c = document.createElement("canvas");
+    c.width = w;
+    c.height = h;
+    c.getContext("2d").drawImage(img, 0, 0, w, h);
 
-    canvas.toBlob(blob => {
+    c.toBlob(blob => {
       const url = URL.createObjectURL(blob);
       const div = document.createElement("div");
       div.className = "result";
-      div.innerHTML = `
-        <img src="${url}">
-        <a href="${url}" download="resized-${file.name}">Download</a>
-      `;
+      div.innerHTML = `<img src="${url}">`;
       results.appendChild(div);
     });
   };
