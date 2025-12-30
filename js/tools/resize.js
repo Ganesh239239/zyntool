8a209b6152a096b43bb14e9ff638c6081f1b3a2c
Ganesh239239 <esportszen239@gmail.com>
2025-12-30 23:26:12 +0530

    Update resize.js

--- js/tools/resize.js
+++ js/tools/resize.js
@@ -1,31 +1,17 @@
-// js/tools/resize.js
-
 export default {
     title: 'Resize IMAGE',
-
     renderUI: (w, h) => `
         <div class="row">
-            <div class="col-6">
-                <label>Width</label>
-                <input type="number" id="w" class="form-control" value="${w}">
-            </div>
-            <div class="col-6">
-                <label>Height</label>
-                <input type="number" id="h" class="form-control" value="${h}">
-            </div>
+            <div class="col-6"><label>Width</label><input type="number" id="w" class="form-control" value="${w}"></div>
+            <div class="col-6"><label>Height</label><input type="number" id="h" class="form-control" value="${h}"></div>
         </div>
     `,
-
-    process: async (imgElement) => {
+    process: async (img) => {
         const w = parseInt(document.getElementById('w').value);
         const h = parseInt(document.getElementById('h').value);
-
-        const canvas = document.createElement('canvas');
-        canvas.width = w;
-        canvas.height = h;
-        const ctx = canvas.getContext('2d');
-        ctx.drawImage(imgElement, 0, 0, w, h);
-
-        return new Promise(resolve => canvas.toBlob(resolve, 'image/png'));
+        const cvs = document.createElement('canvas');
+        cvs.width = w; cvs.height = h;
+        cvs.getContext('2d').drawImage(img, 0, 0, w, h);
+        return new Promise(r => cvs.toBlob(r));
     }
 };
