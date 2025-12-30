0407a7380564e54c2956e9177d530e2fbe64cd85
Ganesh239239 <esportszen239@gmail.com>
2025-12-30 09:16:37 +0530

    Delete js directory

--- js/menu.js
+++
@@ -1,8 +0,0 @@
-const menuBtn = document.getElementById("menuBtn");
-const navMenu = document.getElementById("navMenu");
-
-if (menuBtn && navMenu) {
-  menuBtn.onclick = () => {
-    navMenu.classList.toggle("active");
-  };
-}
