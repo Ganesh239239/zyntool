b01b57b078a09f89071a6225e94fbfc7169a15da
Ganesh239239 <esportszen239@gmail.com>
2025-12-30 13:31:48 +0530

    Create main.js

---
+++ assests/js/main.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('.site-header');
+    if (window.scrollY > 10) {
+        header.style.boxShadow = '0 2px 10px rgba(0,0,0,0.1)';
+    } else {
+        header.style.boxShadow = 'none';
+    }
+});
