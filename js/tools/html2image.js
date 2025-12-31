b1b8dca5c87771981a849a04738c698d9d3b3057
Ganesh239239 <esportszen239@gmail.com>
2025-12-31 12:03:24 +0530

    Delete js directory

--- js/tools/html2image.js
+++
@@ -1,9 +0,0 @@
-export default {
-    title: 'HTML to IMAGE',
-    renderUI: () => `<div class="text-muted">Rendering HTML...</div>`,
-    process: async (elementToRender) => {
-        // This receives the HTML div element instead of an image
-        const canvas = await html2canvas(elementToRender);
-        return new Promise(r => canvas.toBlob(r, 'image/jpeg'));
-    }
-};
