d2269f29f59fa0fffb82621a9e5e9689db6163c2
Ganesh239239 <esportszen239@gmail.com>
2026-01-02 22:29:52 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,11 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-	site: 'https://zyntool.pages.dev',
-	integrations: [react(), tailwind(), sitemap()],
-	output: 'static',
+  site: 'https://zyntool.pages.dev',
+  integrations: [
+    react(), 
+    tailwind({ applyBaseStyles: false }), // applyBaseStyles: false prevents Tailwind from breaking your Native CSS
+    sitemap()
+  ],
+  output: 'static'
 });
