17f7b0db48a1c807edb3261fd2baa14c2206adb1
Ganesh239239 <esportszen239@gmail.com>
2026-01-03 15:37:08 +0530

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
-	theme: {
-		extend: {
-			colors: {
-				brand: '#4f46e5',
-				fontWeight: {
-        black: '900',
-				},
-			},
-		},
-	},
-	corePlugins: {
-		preflight: false, // THIS IS THE KEY: Stops Tailwind from breaking Bootstrap
-	}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
+  corePlugins: {
+    preflight: false, // Prevents Tailwind from overriding your Studio CSS
+  }
 }
